74d6b818232be0acc022a53e3c0ac97043caff21
Ksiyeong <yuiop2767@naver.com>
2023-01-16 23:13:44 +0900

    Feat: 대한통운 크롤링

---
+++ src/main/java/com/eroom/crawlingprac/CrawlingPracApplication.java
@@ -0,0 +1,13 @@
+package com.eroom.crawlingprac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrawlingPracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrawlingPracApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eroom/crawlingprac/CrawlingPracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eroom.crawlingprac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrawlingPracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
